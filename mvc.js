9a60d1eee541f8bc9a09ef83c1246749ec5f67cc
Charles <whitfield.cw@gmail.com>
2015-11-17 23:00:09 -0800

    rewrite ajax request to properly accept user input in url string

--- mvc.js
+++ mvc.js
@@ -2,6 +2,24 @@ var thread = {};
 
 thread.Thread = function(data){
   this.urlPath = m.prop(data.urlPath);
+  currentThread = this
+  currentThread.data = null
+  currentThread.success = currentThread.loading = currentThread.failed = false;
+  this.getRequest = function(){
+    currentThread = currentThread;
+    currentThread.loading = true; currentThread.success = currentThread.failed = false;
+    currentThread.opts = {method:"GET", url:"http://maru-todo.herokuapp.com/tasks"}
+    // currentThread.opts = {method:"GET", url:currentThread.urlPath()}
+    // console.log('in GO')
+    // console.log(currentThread.urlPath())
+    // console.log(currentThread)
+    m.request(currentThread.opts)
+    .then(function(mydata){
+      currentThread.success = true; currentThread.failed = currentThread.loading = false;
+      currentThread.data = mydata;
+      m.redraw();
+    });
+  }
 }
 
 thread.ThreadList = Array;
@@ -15,8 +33,13 @@ thread.vm = (function(){
 
     vm.startThread = function(){
       if (vm.threadSubString()) {
-        fullUrl = "https://reddit.com/r/" + vm.threadSubString() + ".json?sort=new&depth=1&limit=15"
-        vm.list.push(new thread.Thread({urlPath: fullUrl}));
+        fullUrl = "http://reddit.com/r/" + vm.threadSubString() + ".json?sort=new&depth=1&limit=15"
+        var newThread = new thread.Thread({urlPath: fullUrl});
+        console.log('in startThread')
+        console.log(fullUrl)
+        console.log(newThread)
+        newThread.getRequest();
+        vm.list.push(newThread);
         vm.threadSubString("");
       }
     };
@@ -34,16 +57,24 @@ thread.view = function(){
     m("input", {onchange: m.withAttr("value", thread.vm.threadSubString), value: thread.vm.threadSubString()}),
     m("button", {onclick: thread.vm.startThread}, "load thread"),
     m("table", [
-      thread.vm.list.map(spawnThread)
+      thread.vm.list.map(buildThread)
     ])
   ]);
 
-  function spawnThread(thread){
+  function buildThread(newThread){
+    if(!newThread.success){return;}
+    console.log(newThread)
     return m("td", [
-      // m("table", Request.postResponse.data[1].data.children.map(function(commentObj){
-      //         return m("tr", [m("td", commentObj.data.body), m("td", commentObj.data.created), m("td", commentObj.data.author)]);
-      //       }))
-      m("p", thread.urlPath()),
+
+
+
+
+
+    m("table", newThread.data[1].data.children
+      .map(function(commentObj){
+        return m("tr", [m("td", commentObj.data.body), m("td", commentObj.data.created), m("td", commentObj.data.author)]);
+      }))
+      // m("p", thread.urlPath()),
     ])
   }
 }
